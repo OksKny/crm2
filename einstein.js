0209b1375b60c7a1018b3a7cdbfe2ae745a2fe15
OksKny <oksi.kny96@mail.ru>
2023-06-27 16:17:41 +0300

    init

---
+++ einstein.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const myWeight = '65';
+const speedLight = '3e8';
+
+console.log(myWeight * Math.pow(speedLight, 2));
---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const product = 'jeans';
+const quantity = '50';
+const category = 'trousers';
+const price = '2000';
+
+console.log(product);
+console.log(quantity * price);
+console.log(`На складе ${quantity} единиц товара ${product} на сумму ${quantity * price} деревянных`);
+
+
+const productName = prompt('Напишите название товара!');
+const productQuantity = Number(prompt('Напишите количество товара!'));
+const productCategory = prompt('Напишите категорию товара!');
+const productPrice = Number(prompt('Напишите цену товара!'));
+
+console.log(productQuantity);
+console.log(productPrice);
+
+
+
+
