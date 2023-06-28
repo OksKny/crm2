45d2e04ca239e271387ed04aaaf45dcf6498492e
OksKny <oksi.kny96@mail.ru>
2023-06-28 14:58:14 +0300

    init

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const quantity = '50';
 const category = 'trousers';
 const price = '2000';
 
+const productName = prompt('Напишите название товара!');
+const productQuantity = typeof + (prompt('Напишите количество товара!'));
+if (isNaN(productQuantity)) {
+    alert('Вы ввели некорректные данные');
+  } 
+const productCategory = prompt('Напишите категорию товара!');
+const productPrice = typeof + (prompt('Напишите цену товара!'));
+if (isNaN(productPrice)) {
+  alert('Вы ввели некорректные данные');
+} 
+
 console.log(product);
 console.log(quantity * price);
 console.log(`На складе ${quantity} единиц товара ${product} на сумму ${quantity * price} деревянных`);
-
-
-const productName = prompt('Напишите название товара!');
-const productQuantity = Number(prompt('Напишите количество товара!'));
-const productCategory = prompt('Напишите категорию товара!');
-const productPrice = Number(prompt('Напишите цену товара!'));
-
 console.log(productQuantity);
 console.log(productPrice);
 
