f8537a7ab3299f794e9f9b133c5e3c486cf826d9
OksKny <oksi.kny96@mail.ru>
2023-07-04 18:29:46 +0300

    init

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 'use strict';
 
 const productName = prompt('Напишите название товара!');
-const productQuantity = +(prompt('Напишите количество товара!'));
-if (isNaN(productQuantity)) {
-  console.log('Вы ввели некорректные данные');
-  } 
-  
 const productCategory = prompt('Напишите категорию товара!');
+const productQuantity = +(prompt('Напишите количество товара!'));
 const productPrice = +(prompt('Напишите цену товара!'));
-if (isNaN(productPrice)) {
-  console.log('Вы ввели некорректные данные');
-} 
 
-console.log(productName);
-console.log(productQuantity);
-console.log(productPrice);
-console.log(productQuantity * productPrice);
-console.log(`На складе ${productQuantity} единиц товара ${productName} на сумму ${productQuantity * productPrice} деревянных`);
+if (Number.isNaN(productQuantity) || Number.isNaN(productPrice)) {
+  console.log("Вы ввели некоректные данные");
+} else {
+  console.log(`На складе ${productQuantity} единиц товара ${productName} на сумму ${productQuantity * productPrice} деревянных`);
+}
+
+
 
 
 
--- tasks.js
+++ tasks.js
@@ -1,15 +1,23 @@
 'use strict';
-const rain = Math.random();
 const mat = +(prompt('Введите кол-во баллов по математике:'));
 const russ = +(prompt('Введите кол-во баллов по русскому языку:'));
 const info = +(prompt('Введите кол-во баллов по информатике:'));
+
 const bank = +(prompt('Введите сумму, которую хотите снять'));
 
-console.log('Math.round(): ', Math.round(rain));
+const rain = Math.random();
+
+console.log(Math.round(rain));
 if (Math.round(rain) === 1) {
     console.log('Пошёл дождь. Возьмите зонт!');
 } else {
     console.log('Дождя нет!');
+}
+
+if (bank % 100 === 0) {
+    console.log('Заберите деньги');
+} else {
+    console.log('К сожалению, не сможем выдать запрашиваемую сумму. Минимальная купюра, которую можем выдать 100 рублей.');
 }
 
 if (mat + russ + info >= 265) {
@@ -18,11 +26,7 @@ if (mat + russ + info >= 265) {
     console.log('Вы не поступили не бюджет!');
 }
 
-if (bank % 100 == 0) {
-    console.log('Заберите деньги');
-} else {
-    console.log('К сожалению, не сможем выдать запрашиваемую сумму. Минимальная купюра, которую можем выдать 100 рублей.');
-}
+
 
 
 
